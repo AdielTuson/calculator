2189c4a64a6c072fde560088fe3898c0a13878e5
AdielTuson <adielt81@gmail.com>
2023-09-04 17:12:40 +0300

    Write calculator operation function
    This function 'operate', will take two numbers variables and a
    mathematical operator variable and call on one of the operator
    functions according to the operator variable

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Operations functions
+//Mathematical operations functions
 const add = function(a,b) {
     return a+b;
 }
@@ -13,4 +13,27 @@ const multiply = function(a,b) {
 
 const divide = function(a,b) {
     return a/b; 
+}
+
+//Calculator operation
+let variableA;
+let variableB;
+let operator;
+    
+const operate = function(variableA, operator, variableB) {
+    if (operator === '+') {
+        return add(variableA, variableB);
+    } 
+
+    else if (operator === '-') {
+        return subtract(variableA, variableB);
+    }
+
+    else if (operator === '*') {
+        return multiply(variableA, variableB);
+    }
+
+    else if (operator === '/') {
+        return divide(variableA,variableB);
+    }
 }
